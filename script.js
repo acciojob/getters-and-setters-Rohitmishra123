a0d366b217b62840af20775d2796d1a0831bd99b
Rohit Kumar Mishra <105963356+Rohitmishra123@users.noreply.github.com>
2026-02-15 00:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,23 +7,17 @@ class Person {
     this._name = name;
     this._age = age;
   }
-
-  // Getter for name
   get name(): string {
     return this._name;
   }
-
-  // Setter for name
   set name(newName: string) {
     this._name = newName;
   }
-
-  // Getter for age
   get age(): number {
     return this._age;
   }
 
-  // Setter for age
+ 
   set age(newAge: number) {
     this._age = newAge;
   }
