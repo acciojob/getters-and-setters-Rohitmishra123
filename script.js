c3c529293e5808f0fb5d966a0540fddaadc2a890
Rohit Kumar Mishra <105963356+Rohitmishra123@users.noreply.github.com>
2026-02-15 00:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 //complete this code
-class Person {}
+class Person {
+  private _name: string;
+  private _age: number;
 
-class Student extends Person {}
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Teacher extends Person {}
+  // Getter for name
+  get name(): string {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName: string) {
+    this._name = newName;
+  }
+
+  // Getter for age
+  get age(): number {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(newAge: number) {
+    this._age = newAge;
+  }
+}
+
+class Student extends Person {
+  study(): void {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach(): void {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
